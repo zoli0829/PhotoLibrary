aa01eefcbc0ffe6697c822e88dd0bb2515ef7a3c
zoli0829 <zoli0829@gmail.com>
2025-05-15 05:18:06 +0100

    Initial commit

---
+++ PhotoLibrary/PhotoLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLibraryApp.swift
+//  PhotoLibrary
+//
+//  Created by Zoltan Vegh on 15/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
